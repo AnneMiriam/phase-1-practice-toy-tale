a5adde9f394acfc29e47b92a236bfac1ae5e10e0
Anne Hastings <anne.miriam@gmail.com>
2023-11-06 21:10:04 -0700

    complete lab

--- src/index.js
+++ src/index.js
@@ -13,3 +13,73 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+const toyUrl = 'http://localhost:3000/toys';
+const toyCollection = document.getElementById('toy-collection');
+const submitToy = document.querySelector('.submit')
+
+
+function renderCard(toys) {
+  const toyCard = document.createElement('div');
+  toyCard.className = "card";
+  toyCard.id = `toy-${toys.id}`;
+  const h2 = document.createElement('h2');
+  h2.textContent = toys.name;
+  const img = document.createElement('img');
+  img.src = toys.image;
+  img.alt = toys.name;
+  img.className = "toy-avatar"
+  const p = document.createElement('p');
+  p.textContent = `${toys.likes} Likes`;
+  const btn = document.createElement('button');
+  btn.textContent = "Like ❤️"
+  btn.className = "like-btn"
+  btn.id = toys.id
+  btn.addEventListener('click', (e) => {
+    fetch(`${toyUrl}/${toys.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body : JSON.stringify({likes: ++toys.likes})
+    })
+    .then(resp => resp.json())
+    .then(updateCard => {
+      e.target.parentElement.querySelector('p').textContent = `${updateCard.likes} Likes`
+    })
+    .catch(err => alert(err))
+  })
+  toyCard.append(h2, img, p, btn);
+  toyCollection.append(toyCard)
+}
+
+  fetch(toyUrl)
+  .then(resp => resp.json())
+  .then(toyArr => {
+    toyArr.forEach(toyData => renderCard(toyData))
+  }) 
+  .catch(err => alert(err))
+
+  const addToyForm = document.querySelector('.add-toy-form')
+
+  addToyForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const image = e.target.image.value;
+    if (name.trim() && image.trim()) {
+      fetch(toyUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({name, image, likes:0})
+      })
+      .then(resp => resp.json())
+      .then(createToy => renderCard(createToy))
+      .catch(err => alert(err))
+      e.target.reset();
+    } else {
+      alert("Please fill the full form")
+    }
+  })
